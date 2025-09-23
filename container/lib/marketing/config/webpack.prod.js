d36f0bb3f2d3415282163735ed788fabe43824de
ahmed7alwakeel <alwakeel1996@gmail.com>
2025-09-23 15:53:01 +0300

    Add production webpack configuration and GitHub Actions workflow for deployment

---
+++ container/lib/marketing/config/webpack.prod.js
@@ -0,0 +1,29 @@
+const { merge } = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const commonConfig = require('./webpack.common');
+const packageJson = require('../package.json');
+const domain = process.env.PRODUCTION_DOMAIN;
+
+const prodConfig = {
+    mode: 'production',
+    output: {
+        //any file will be fileName.hash.js
+        filename: '[name].[contenthash].js',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+        }),
+        new ModuleFederationPlugin({
+            name: 'marketing',
+            filename: 'remoteEntry.js',
+            exposes: {
+                './MarketingApp': './src/bootstrap',
+            },
+            shared: packageJson.dependencies,
+        }),
+    ],
+};
+
+module.exports = merge(commonConfig, prodConfig);
