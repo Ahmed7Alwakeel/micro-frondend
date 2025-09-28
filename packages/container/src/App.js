593d0dd46b917db4997b6fe1f5d16fe31e7c101b
ahmed7alwakeel <alwakeel1996@gmail.com>
2025-09-28 04:13:48 +0300

    feat: update App component to navigate to home on header click

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import MarketingApp from './components/MarketingApp';
+import { useNavigate } from "react-router-dom";
 
 export default () => {
+  const navigate = useNavigate();
   return (
     <div>
-      <h1>Hi there!</h1>
+      <h1 onClick={() => navigate("/")}>App</h1>
       <hr />
       <MarketingApp />
     </div>
